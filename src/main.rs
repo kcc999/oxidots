d980032a5dc4139fe3d2d96bfa8ab2b73156267b
kcc999 <karolc9991@gmail.com>
2025-08-03 18:20:01 +0200

    adds libgit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs::{self}, io, path::Path, sync::mpsc};
 
 use clap::Parser;
+use git2::{IndexAddOption, Repository};
 use log::LevelFilter;
 use notify::{Event, Result, RecursiveMode, Watcher};
 use simplelog::{Config, WriteLogger};
@@ -13,6 +14,43 @@ struct Cli {
 
 pub fn verify(file_path: &str) -> bool {
     Path::new(file_path).is_dir()
+}
+
+pub fn git_sync(user_dotfiles: &str) {
+    let repo = match Repository::open(user_dotfiles) {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to open: {}" ,e),
+    };
+
+    let mut index = repo.index().unwrap();
+
+    let _ = index.add_all(["."].iter(), IndexAddOption::DEFAULT, None);
+    let _ = index.write();
+
+    println!("DEBUG: Index has {} entries", index.len());
+
+    let tree_id = index.write_tree().unwrap();
+    let tree = repo.find_tree(tree_id).unwrap();
+
+    let parent_commit = match repo.head() {
+        Ok(head) => {
+            let target = head.target().unwrap();
+            Some(repo.find_commit(target).unwrap())
+        }
+        Err(_) => None, // No previous commits (initial commit)
+    };
+
+    let message: &str = "Oxidots: update";
+
+    let commit = repo.commit(Some("HEAD"), 
+        &repo.signature().unwrap(),
+        &repo.signature().unwrap(),
+        message,
+        &tree,
+        &[&parent_commit.unwrap()] // @TODO: Will panic if no parent commit
+        );
+
+    println!("DEBUG COMMIT {:?}", commit);
 }
 
 fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
@@ -38,15 +76,15 @@ fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> 
 
 pub fn initial_sync(watch_files: &Vec<String>, user_dotfiles: &str) {
     for f in watch_files {
-        let directory_name = f.split("/").last().unwrap();
-        let new_path = user_dotfiles.to_owned() + "/" + directory_name;
+        let src_path = Path::new(f);
+        let dir_name = src_path.file_name().unwrap();
+        let dst_path = Path::new(user_dotfiles).join(dir_name);
 
-        log::info!("Copying {:?} to {:?}", f.as_str(), new_path.as_str());
-        let _ = copy_dir_all(Path::new(f.as_str()), Path::new(new_path.as_str()));
+        let _ = copy_dir_all(src_path, dst_path);
     }
 }
 
-pub fn get_watch_files(config_file_path: &str) -> Vec<String> {
+pub fn get_watch_dirs(config_file_path: &str) -> Vec<String> {
     let content = match fs::read_to_string(config_file_path) {
         Ok(content) => content,
         Err(e) => {
@@ -55,7 +93,9 @@ pub fn get_watch_files(config_file_path: &str) -> Vec<String> {
         }
     };
 
-    let watch_files: Vec<String> = content.lines().map(|s| s.to_string()).collect();
+    let watch_files: Vec<String> = content.lines()
+        .map(|s| s.to_string())
+        .collect();
 
     for file in watch_files.iter() {
         log::info!("Verifying file {:?}", file);
@@ -66,6 +106,7 @@ pub fn get_watch_files(config_file_path: &str) -> Vec<String> {
     
     return watch_files;
 }
+
 fn main() -> Result<()> {
     WriteLogger::init(
         LevelFilter::Info,
@@ -74,14 +115,14 @@ fn main() -> Result<()> {
     ).unwrap();
 
     let cli = Cli::parse();
-    let watch_files: Vec<String> = get_watch_files(cli.config_file.as_str());
+    let watch_dirs: Vec<String> = get_watch_dirs(cli.config_file.as_str());
 
     let (tx, rx) = mpsc::channel::<Result<Event>>();
     let mut watcher = notify::recommended_watcher(tx)?;
     
-    initial_sync(&watch_files, cli.user_dotfiles.as_str());
+    initial_sync(&watch_dirs, cli.user_dotfiles.as_str());
 
-    for file in watch_files.iter() {
+    for file in watch_dirs.iter() {
         log::info!("Watching --> {:?}", file.as_str());
         watcher.watch(Path::new(file.as_str()), RecursiveMode::NonRecursive)?;
     }
@@ -91,6 +132,7 @@ fn main() -> Result<()> {
             Ok(event) => {
                 if event.kind == notify::EventKind::Modify(notify::event::ModifyKind::Data(notify::event::DataChange::Content)) {
                     log::info!("Modified file: {:?}", event.paths.get(0));
+                    git_sync(&cli.user_dotfiles);
                 } 
             }
             Err(e) => log::error!("watch error: {:?}", e),
